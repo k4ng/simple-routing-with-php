2768479a98ed7aea975e2997f5c6f45402bf7345
dyazincahya <cahyadyazin@yahoo.com>
2019-12-29 13:07:37 +0700

    first up to git

---
+++ index.php
@@ -0,0 +1,23 @@
+<?php
+    // Define your location project directory in htdocs (EX THE FULL PATH: D:\xampp\htdocs\x-kang\simple-rooting-with-php)
+    $project_location = "/x-kang/simple-rooting-with-php";
+    $me = $project_location;
+
+    // For get URL PATH
+    $request = $_SERVER['REQUEST_URI'];
+
+    switch ($request) {
+        case $me.'/' :
+            require "views/home.php";
+            break;
+        case $me.'/about' :
+            require "views/about.php";
+            break;
+        case $me.'/contact' :
+            require "views/contact.php";
+            break;
+        default:
+            http_response_code(404);
+            echo "404";
+            break;
+    }
---
+++ views/home.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Home - Simple rooting with PHP</title>
+</head>
+<body>
+    <h1>This is home page :)</h1>
+</body>
+</html>
